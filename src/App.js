37dfcb6818c9776e883d52ec6b74d24d529d95ab
Alecsandra Brewer <alecsandrabrewer@yahoo.com>
2021-03-21 22:47:43 -0700

    order form done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Link, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import OrderForm from './OrderForm';
+
+// Styling 
+const DivStyled1 = styled.div `
+  display:flex;
+  background: maroon;
+  justify-content:center;
+`
+const Header = styled.h1`
+  font-size:3rem;
+  color: white;
+`
+const NavLinks = styled.div`
+  border:2px solid black;
+  background:white;
+  padding: 2%;
+  margin: 0% 0% 5%;
+  display:flex;
+  justify-content: center;
+  justify-content: space-evenly;
+`
+const DivStyled2 = styled.div`
+  padding: 3%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: #C0BDB5;
+`
+
+// Form 
+
+const initialFormValues = {
+  //text input
+  name: '',
+  //dropdown
+  size: '',
+  //radio button
+  sauce: '',
+  //checkboxes
+  cheese: false,
+  pepperoni: false,
+  sausage: false, 
+  mushrooms: false, 
+  bacon: false, 
+  //text input
+  instructions: '',
+}
+
 
 const App = () => {
+  const [pizzaOrder, setPizzaOrder] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  //update and submit handlers
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue})
+  }
+  const submitForm = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      sauce: formValues.sauce.trim(),
+      toppings: ['cheese', 'pepperoni', 'sausage', 'mushrooms', 'onions', 'bacon'].filter(toppings => formValues[toppings]),
+      instructions: formValues.instructions.trim()
+    }
+  }
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <DivStyled1>
+        <nav>
+          <Header>Lambda Eats</Header> 
+          <NavLinks>
+            <Link to='/'>Home</Link>
+            <Link to='/pizza'>Order Pizza</Link>
+          </NavLinks> 
+          <Switch>
+            <Route exact path='/'/>
+          </Switch>
+        </nav>
+      </DivStyled1>
+      <DivStyled2>
+        <h2> Build Your Own Pizza!</h2>
+        <OrderForm
+          values = {formValues}
+          update = {updateForm}
+          submit = {submitForm}
+        />
+      </DivStyled2>
+    </div>
   );
 };
 export default App;
---
+++ src/OrderForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+//styling
+const FormStyled = styled.form`
+    display:flex;
+    flex-direction:column;
+    padding: 2%;
+`
+const LabelStyled = styled.label`
+    margin: 3%;
+    font-size:1.2rem;
+    display:flex;
+    flex-direction: column;
+`
+const RowStyled = styled.label` 
+    display:flex;
+    justify-content:center;
+`
+
+const OrderForm = (props) => {
+    const {values, submit, update} = props;
+
+    const onChange = evt => {
+        const {name, value, checked, type} = evt.target
+        const val = type === 'checkbox' ? checked : value
+        update (name, value)
+        
+    }
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
+    return (
+        <FormStyled className='form-container' onSubmit={onSubmit}>
+            <LabelStyled>Your Name:
+                <input type='text' value={values.name} placeholder='Full Name' name='name' maxLength='30' onChange={onChange}/>
+            </LabelStyled>
+            <LabelStyled>Size:
+                <select name='size' onChange={onChange} value={values.size}>
+                    <option>Personal</option>
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+            </LabelStyled>
+            <h3>Choose Your Sauce!</h3>
+            <RowStyled>Red sauce
+                <input type='radio' name='sauce' onChange={onChange} value='redSauce' checked={values.sauce === 'redSauce'}/>
+            </RowStyled>
+            <RowStyled>White Sauce
+                <input type='radio' name='sauce' onChange={onChange} value='whiteSauce' checked={values.sauce === 'whiteSauce'}/>
+            </RowStyled>
+            <h3>Select Your Toppings!</h3>
+            <RowStyled>Cheese
+                <input type='checkbox' name='cheese' onChange={onChange} value={values.cheese}/>
+            </RowStyled>
+            <RowStyled>Pepperoni
+                <input type='checkbox' name='pepperoni' onChange={onChange} value={values.pepperoni}/>
+            </RowStyled>
+            <RowStyled>Sausage
+                <input type='checkbox' name='sausage' onChange={onChange} value={values.sausage}/>
+             </RowStyled>
+            <RowStyled>Mushrooms
+                <input type='checkbox' name='mushrooms' onChange={onChange} value={values.mushrooms}/>
+            </RowStyled>
+            <RowStyled>Bacon
+                <input type='checkbox' name='bacon' onChange={onChange} value={values.bacon}/>
+            </RowStyled>
+            <LabelStyled>
+                <h3>Special Instructions:</h3>
+                <input type='text' value={values.instructions} placeholder='type here' name='instructions' maxLength='60' onChange={onChange}/> 
+            </LabelStyled>
+            
+        </FormStyled>
+    )
+}
+
+export default OrderForm;
